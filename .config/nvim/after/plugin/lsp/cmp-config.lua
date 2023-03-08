0ad931d468565812254c5399fdc2fdc08c010073
Tio-Panda <sebastian.gutierrez.milla@gmail.com>
2023-03-08 01:03:24 -0300

    feat: Desactivate copilot & true-zen

--- .config/nvim/after/plugin/lsp/cmp-config.lua
+++ .config/nvim/after/plugin/lsp/cmp-config.lua
@@ -75,8 +75,8 @@ cmp.setup({
     sorting = {
         priority_weight = 2,
         comparators = {
-            require("copilot_cmp.comparators").prioritize,
-            require("copilot_cmp.comparators").score,
+            -- require("copilot_cmp.comparators").prioritize,
+            -- require("copilot_cmp.comparators").score,
 
             -- Below is the default comparitor list and order for nvim-cmp
             cmp.config.compare.offset,
