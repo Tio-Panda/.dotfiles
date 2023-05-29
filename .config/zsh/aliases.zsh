365b4502cd3a88d6b03f529a38b04d2709f3ed21
Tio-Panda <sebastian.gutierrez.milla@gmail.com>
2023-05-28 23:39:25 -0400

    feat: add more git alias

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -9,26 +9,36 @@ alias h="tmux-sessionizer home"
 alias zsh-update-plugins="find "$ZDOTDIR/plugins" -type d -exec test -e '{}/.git' ';' -print0 | xargs -I {} -0 git -C {} pull -q"
 
 # Shortcuts to config files
-#alias nrc='~/.scripts/tmux/tmux-sessionizer ~/.config/nvim'
 alias nrc='tmux-sessionizer nrc'
 alias zrc='tmux-sessionizer zrc'
 alias trc='tmux-sessionizer trc'
 alias arc='tmux-sessionizer arc'
 
-# alias lvim='nvim -u ~/.local/share/lunarvim/lvim/init.lua --cmd "set runtimepath+=~/.local/share/lunarvim/lvim"'
-
 # Git alias
+#TODO: Add alias for moving between known branches with worktrees
 alias m="git checkout master"
 alias s="git checkout stable"
 
 alias gl="git log"
 alias gs="git status"
-alias gc="git clone --bare"
+alias gcb="git clone --bare"
 
 alias gta="git worktree add"
 alias gtr="git worktree remove --force"
 
-alias p="git pull"
+alias ga="git add"
+
+alias gc="git commit -m"
+alias gca="git commit -a -m"
+
+alias pl="git pull"
+alias ps="git push"
+
+alias g='(){ git commit -a -m "$1"; git push }'
+
+#TODO: Add alias for diff and solve commits diff
+
+alias gcundo="git reset --soft HEAD~"
 
 # Colorize grep output (good for log files)
 alias grep='grep --color=auto'
