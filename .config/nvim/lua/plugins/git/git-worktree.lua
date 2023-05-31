b9cd0cc121d7d8facee0cb34b5658044558424f1
Tio-Panda <sebastian.gutierrez.milla@gmail.com>
2023-05-31 01:31:54 -0400

    add quicksetup script and qol script for ssh keys

--- .config/nvim/lua/plugins/git/git-worktree.lua
+++ .config/nvim/lua/plugins/git/git-worktree.lua
@@ -6,6 +6,8 @@ end
 map("n", "<leader>gt", ":lua require('telescope').extensions.git_worktree.git_worktrees()<cr>", '[g]it [w]orktrees menu')
 map("n", "<leader>gta", ":lua require('telescope').extensions.git_worktree.create_git_worktree()<cr>", '[g]it [w]orktree [a]dd')
 
+-- TODO: Encontrar un uso para los git-worktree hooks, youtu.be/2uEqYw-N8uE?t=330 para tomar de ejemplo
+
 return {
     change_directory_command = "cd",  -- default: "cd",
     update_on_change = true,          -- default: true,
